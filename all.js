f182566d4ce555c4b72edb69e1b797f1bf681bec
dahnote <dahnote1993@gmail.com>
2021-04-05 12:50:31 +0800

    內容取值

--- all.js
+++ all.js
@@ -1,6 +1,13 @@
 // console.log('123')
 const title=document.querySelector(".title");
 title.addEventListener("click",function(e){
-    console.log(e.target.textContent)
-    console.log(e.target.getAttribute("class"))
+    // console.log(e.target.textContent)
+    // console.log(e.target.getAttribute("class"))
+    // console.log(e.target.nodeName)
+    // 確認點選到的是input
+    if (e.target.nodeName!=='INPUT'){
+        return
+    }
+    console.log('點選成功')
+    console.log(e.target.getAttribute("value"))
 })
