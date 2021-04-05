755aef674bffffc175ecc5ac915741925a3d6a26
dahnote <dahnote1993@gmail.com>
2021-04-05 15:20:17 +0800

    data- 取值

--- all.js
+++ all.js
@@ -11,15 +11,18 @@
 //     console.log('點選成功')
 //     console.log(e.target.getAttribute("value"))
 // })
-const box=document.querySelector(".box");
-// console.log(box)
-box.addEventListener("click",function(e){
-    // console.log(e.target.nodeName)
-    // console.log(e.target.getAttribute("value"))
-    // if (e.target.getAttribute("class")==="view"){
-    //     console.log(e.target.getAttribute("value"))
-    // }
-    if (e.target.nodeName==="INPUT"){
-        console.log(e.target.getAttribute("value"))
-    }
-})
+// const box=document.querySelector(".box");
+// // console.log(box)
+// box.addEventListener("click",function(e){
+//     // console.log(e.target.nodeName)
+//     // console.log(e.target.getAttribute("value"))
+//     // if (e.target.getAttribute("class")==="view"){
+//     //     console.log(e.target.getAttribute("value"))
+//     // }
+//     if (e.target.nodeName==="INPUT"){
+//         console.log(e.target.getAttribute("value"))
+//     }
+// })
+
+const title=document.querySelector(".title");
+console.log(title.getAttribute("data-num"))
