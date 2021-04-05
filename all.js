b7e3adddf4d8e5c6eca4b30d8acadab6cf41c0b1
dahnote <dahnote1993@gmail.com>
2021-04-05 15:12:43 +0800

    大範圍取值

--- all.js
+++ all.js
@@ -1,13 +1,25 @@
 // console.log('123')
-const title=document.querySelector(".title");
-title.addEventListener("click",function(e){
-    // console.log(e.target.textContent)
-    // console.log(e.target.getAttribute("class"))
+// const title=document.querySelector(".title");
+// title
+//     // console.log(e.target.textContent)
+//     // console.log(e.target.getAttribute("class"))
+//     // console.log(e.target.nodeName)
+//     // 確認點選到的是input
+//     if (e.target.nodeName!=='INPUT'){
+//         return
+//     }
+//     console.log('點選成功')
+//     console.log(e.target.getAttribute("value"))
+// })
+const box=document.querySelector(".box");
+// console.log(box)
+box.addEventListener("click",function(e){
     // console.log(e.target.nodeName)
-    // 確認點選到的是input
-    if (e.target.nodeName!=='INPUT'){
-        return
+    // console.log(e.target.getAttribute("value"))
+    // if (e.target.getAttribute("class")==="view"){
+    //     console.log(e.target.getAttribute("value"))
+    // }
+    if (e.target.nodeName==="INPUT"){
+        console.log(e.target.getAttribute("value"))
     }
-    console.log('點選成功')
-    console.log(e.target.getAttribute("value"))
 })
